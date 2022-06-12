251fe718988620328d22d3e34cdd322729d76016
Donna <donnajun1214@gmail.com>
2022-06-13 08:49:39 +0900

    Initial Commit

---
+++ I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by Donna on 2022/06/13.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
